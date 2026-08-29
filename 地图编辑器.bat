3b8d4dd6ef425d431bd4b8f1bc66d9c8cc1259a7
qing99half <qing99half@users.noreply.github.com>
2026-08-29 11:07:06 +0800

    fix: 编辑器启动脚本路径尾杠吞引号导致闪退(pushd取无尾杠路径)

--- 地图编辑器.bat
+++ 地图编辑器.bat
@@ -1,3 +1,6 @@
 @echo off
 rem 双击启动地图编辑器(窗口模式)
-start "" "D:\Godot_v4.7.2-stable_mono_win64\Godot_v4.7.2-stable_mono_win64.exe" --path "%~dp0" "res://src/editor/MapEditor.tscn"
+rem 注意:%~dp0 自带末尾反斜杠,放进引号参数会吞掉引号,故用 pushd 取无尾杠路径
+pushd %~dp0
+start "" "D:\Godot_v4.7.2-stable_mono_win64\Godot_v4.7.2-stable_mono_win64.exe" --path "%CD%" "res://src/editor/MapEditor.tscn"
+popd
